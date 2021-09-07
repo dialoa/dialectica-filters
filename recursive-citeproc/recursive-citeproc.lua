12b6a4373c8b5c9b40046003211113e01409fec3
jdutant <34026710+jdutant@users.noreply.github.com>
2021-09-07 16:08:03 +0100

    fixes typo

--- recursive-citeproc/recursive-citeproc.lua
+++ recursive-citeproc/recursive-citeproc.lua
@@ -148,7 +148,7 @@ function recursive_citeproc(document)
 
     -- check whether citations have been added (or nocite contains @*)
     local new_all_cites, nocite_cites = collect_citations_ids(new_doc)
-    if nocite_cites:find('*') or #new_all_cites == #all_cites then
+    if nocite_cites:find('*')or #new_all_cites == #all_cites then
         new_doc.meta['suppress-bibliography'] = true
         return new_doc
     else
@@ -186,6 +186,8 @@ end
 -- # Main filters
 
 return {
-        Meta = get_options
+    {
+        Meta = get_options,
         Pandoc = recursive_citeproc
+    }
 }
